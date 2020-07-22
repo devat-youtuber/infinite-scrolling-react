ad0b4bebb2e44d95de799a2535e6728bf5a588f6
dev at <battuosai01@gmail.com>
2020-07-22 19:42:23 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,73 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect, useRef} from 'react';
+import Loading from './loading.gif'
+
 
 function App() {
+  const [photos, setPhotos] = useState([])
+  const [pageNumber, setPageNumber] = useState(1)
+  const [loading, setLoading] = useState(false)
+
+
+  const fetchPhotos = async(pageNumber) =>{
+    const Access_Key = "VaS3ud1C-0gdW1nw41FbBryaV_Q5obZ04o-3Vi2QC1E"
+    const res = await fetch(`https://api.unsplash.com/photos/?client_id=${Access_Key}&page=${pageNumber}&per_page=10`)
+    const data = await res.json()
+    // console.log(data)
+    setPhotos(p => [...p, ...data])
+    setLoading(true)
+  }
+
+  useEffect(()=>{
+    fetchPhotos(pageNumber);
+  },[pageNumber])
+
+  const loadMore = () =>{
+    setPageNumber(prevPageNumber => prevPageNumber + 1)
+  }
+
+  const pageEnd = useRef();
+  let num = 1;
+  
+  useEffect(()=>{
+    if(loading){
+      const observer = new IntersectionObserver(entries =>{
+        if(entries[0].isIntersecting){
+          num++;
+          loadMore();
+          if(num >= 10){
+            observer.unobserve(pageEnd.current)
+          }
+        }
+
+      },{threshold: 1});
+
+      observer.observe(pageEnd.current)
+
+    }
+
+  },[loading,num])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Infinite scrolling react hooks</h1>
+      {
+        photos.map((photo,index) =>(
+          <div className="photos" key={index}>
+            <img src={photo.urls.small} alt=""/>
+            <p>{photo.user.first_name + ' ' + photo.user.last_name}</p>
+            <span>Like: {photo.user.total_likes}</span>
+          </div>
+        ))
+      }
+      <div className="loading">
+        <img src={Loading} alt=""/>
+      </div>
+
+      <h3>{photos.length}</h3>
+
+      <button onClick={loadMore} ref={pageEnd}>
+        Load More
+      </button>
     </div>
   );
 }
